d182f3d1e1ea8cc92638428d45de049057023d4c
chris <chris@bumblehead.com>
2013-03-29 01:27:13 -0700

    updated tests

--- test/ScroungeElement.spec.js
+++ test/ScroungeElement.spec.js
@@ -98,7 +98,7 @@ describe("getElemIncludeTplStr", function () {
 });
 
 
-describe("getElemScroungeTplStr", function () {
+describe("getScroungeElemTplStr", function () {
 
   it("should return correct scrounge element, js", function () {
     var str = markupStr_scroungeElements_treesNone_typeJS_valid,
@@ -170,21 +170,132 @@ describe("getFromStrTreesArr", function () {
     '   <!-- </scrounge> -->',
       treeArr;
 
-  it("should return trees from string with one scrounge element", function () {
+  it("should return 1 trees from string with 1 scrounge elements, 1 trees", function () {
     treeArr = ScroungeElement.getFromStrTreesArr(strTrees_attrOne_treeOne);
     expect( treeArr[0] === 'one').toBe(true);
   });
 
-  it("should return trees from string with two scrounge elements", function () {
+  it("should return 2 trees from string with 2 scrounge elements, 2 trees", function () {
     treeArr = ScroungeElement.getFromStrTreesArr(strTrees_attrTwo_treeTwo);
     expect( treeArr[0] === 'one' && treeArr[1] === 'two').toBe(true);
   });
 
-  it("should return no trees from string with two scrounge elements and no trees", function () {
+  it("should return 0 trees from string with 2 scrounge elements, 0 trees", function () {
     treeArr = ScroungeElement.getFromStrTreesArr(strTrees_attrOne_treeNone);
     expect( treeArr.length === 0 ).toBe(true);
   });
 
-
+  it("should return 0 trees from string with 1 scrounge elements, 0 trees", function () {
+    treeArr = ScroungeElement.getFromStrTreesArr(strTrees_attrOne_treeNone);
+    expect( treeArr.length === 0 ).toBe(true);
+  });
 
 });
+
+
+describe("isTreeMatch", function () {
+  var scroungeElemObj, treeObj;
+  
+  scroungeElemObj = ScroungeElement.getNew({
+    trees : ['treeOne', 'treeTwo'],
+    type : 'css'
+  });
+
+  it("should return false when treename not defined in scroungeElem", function () {
+    treeObj = {
+      treename : 'treeThree',
+      type : 'css'
+    };
+
+    expect( scroungeElemObj.isTreeMatch(treeObj) ).toBe(false);
+  });
+
+
+  it("should return true when treename is defined in scroungeElem", function () {
+    treeObj = {
+      treename : 'treeTwo',
+      type : 'css'
+    };
+
+    expect( scroungeElemObj.isTreeMatch(treeObj) ).toBe(true);
+  });
+
+  it("should return false when type is not defined in scroungeElem", function () {
+    treeObj = {
+      treename : 'treeTwo',
+      type : 'js'
+    };
+
+    expect( scroungeElemObj.isTreeMatch(treeObj) ).toBe(false);
+  });
+});
+
+describe("getTreeAttributeStr", function () {
+  var scroungeElemObj, treeAttr;
+
+  it("should return a correct treeAttribute for 2 trees", function () {
+    scroungeElemObj = ScroungeElement.getNew({
+      trees : ['treeOne', 'treeTwo']
+    });  
+
+    treeAttr = scroungeElemObj.getTreeAttributeStr();
+
+    expect( treeAttr ).toBe( ' trees="treeOne,treeTwo"' );
+  });
+
+  it("should return a correct treeAttribute for 1 trees", function () {
+    scroungeElemObj = ScroungeElement.getNew({
+      trees : ['treeOne']
+    });  
+
+    treeAttr = scroungeElemObj.getTreeAttributeStr();
+
+    expect( treeAttr ).toBe( ' trees="treeOne"' );
+  });
+
+  it("should return a correct treeAttribute for 0 trees", function () {
+    scroungeElemObj = ScroungeElement.getNew({
+      trees : []
+    });  
+
+    treeAttr = scroungeElemObj.getTreeAttributeStr();
+
+    expect( treeAttr ).toBe( '' );
+  });
+
+});
+
+describe("getScroungeElemStr", function () {
+  var scroungeElemObj, scroungeElemStr, finStr;
+  it("should return a correct element with includeElemsStr", function () {
+    scroungeElemObj = ScroungeElement.getNew({
+      type : 'js'
+    });
+
+    scroungeElemStr = scroungeElemObj.getScroungeElemStr('__str__');
+    finStr = '' +
+      '<!-- <scrounge.js> -->' +
+      '\n__str__\n' + 
+      '<!-- </scrounge> -->';      
+
+    expect( scroungeElemStr ).toBe( finStr );
+  });
+
+
+  it("should return a correct element without includeElemsStr", function () {
+    scroungeElemObj = ScroungeElement.getNew({
+      type : 'js'
+    });
+
+    scroungeElemStr = scroungeElemObj.getScroungeElemStr('');
+    finStr = '' +
+      '<!-- <scrounge.js> -->' +
+      '\n\n' + 
+      '<!-- </scrounge> -->';      
+
+    expect( scroungeElemStr ).toBe( finStr );
+  });
+
+});
+
+
