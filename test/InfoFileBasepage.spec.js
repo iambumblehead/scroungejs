102ec335f9db5cc918cbd65606372dca77d780a9
bumblehead <chrisdep@FNG117021.local>
2013-06-04 19:01:42 -0700

    begun adding more basepage tests

--- test/InfoFileBasepage.spec.js
+++ test/InfoFileBasepage.spec.js
@@ -2,6 +2,27 @@ var BasepageUtil = require('../lib/fileInfo/fileInfoBasepage');
 
 var basepageStr_valid = 'string';
 
+var basepageStr_scroungeElem_empty = '' +
+      '    <!-- <scrounge.js> -->\n' +
+      '    <!-- </scrounge> -->\n  ';
+
+var basepageStr_scroungeElem_empty_0trees = '' +
+      '    <!-- <scrounge.js> -->\n' +
+      '    <!-- </scrounge> -->\n  ';
+
+var basepageStr_scroungeElem_empty_2trees = '' +
+      '    <!-- <scrounge.js trees="treeA.js, treeB.js"> -->\n' +
+      '    <!-- </scrounge> -->\n  ';
+
+var basepageStr_scroungeElem_full = '' +
+      '    <!-- <scrounge.js> -->\n' +
+      '    <script src="/cmpr/library.js" type="text/javascript"></script>\n' +
+      '    <script src="/cmpr/app2.js" type="text/javascript"></script>\n' +
+      '    <script src="/cmpr/app.js" type="text/javascript"></script>\n' +
+      '    <!-- </scrounge> -->\n  ';
+
+
+
 describe("FileInfoBasepage.getNew", function () {
   var basepageUtil;// = BasepageUtil.getNew({ basepage : 'testbasepage' });
   
@@ -22,12 +43,11 @@ describe("FileInfoBasepage.getNew", function () {
 
 describe("fileInfoBasepage.getScroungeElementObjArr", function () {
   var basepageUtil = BasepageUtil.getNew({ basepage : 'testbasepage' });
-  
-  basepageUtil.getContentStr = function (fn) {
-    fn(null, basepageStr_valid);
-  };
 
   it("should return filters from a valid basepage", function (done) {
+    basepageUtil.getContentStr = function (fn) {
+      fn(null, basepageStr_valid);
+    };
     basepageUtil.getFilters(function (err, filters) {
       expect( true ).toBe( true );
       done();
