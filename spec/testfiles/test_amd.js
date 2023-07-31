a4c83e0f93148ae36db979e41886a0ec16a6942e
chris <chris@bumblehead.com>
2023-07-31 11:03:23 -0700

    remove amd-format from test files, not needed

--- spec/testfiles/test_amd.js
+++
@@ -1,5 +0,0 @@
-// Filename: test_amd.js
-// Timestamp: 2018.04.07-15:09:41 (last modified)
-// Author(s): bumblehead <chris@bumblehead.com>
-
-define(['dep'] , function (d) { return function () {}; });
--- test/build.js
+++ test/build.js
@@ -5,17 +5,22 @@
 import scroungejs from '../src/scrounge.js'
 import express from 'express'
 import http from 'http'
+import url from 'url'
 import path from 'path'
+
+const __filename = new url.URL('', import.meta.url).pathname
+const __dirname = __filename.replace(/[/\\][^/\\]*?$/, '')
 
 const port = 3456
 const app = express()
 
 scroungejs.build({
-  inputpath : './src',
-  outputpath : './out',
-  publicpath : './out/',
-  basepagein : './index.tpl.html',
-  basepage : './index.html',
+  inputpath : './test/src',
+  outputpath : './test/out',
+  publicpath : './test/out/',
+  basepagein : './test/index.tpl.html',
+  // basepagein : __dirname + '/index.tpl.html',
+  basepage : './test/index.html',
   iscompress : false,
   isconcat : false,
   treearr : [ 'app.js', 'app.css' ],
