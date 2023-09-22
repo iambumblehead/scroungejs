56567d509da8a361c9df0b226b54e522396d9e65
chris <chris@bumblehead.com>
2023-09-21 22:31:14 -0700

    start transition from callback to async await

--- src/scrounge_adapt.js
+++ src/scrounge_adapt.js
@@ -14,146 +14,145 @@ import replaceimports from 'replace-imports'
 
 import scrounge_uid from './scrounge_uid.js'
 
-export default (o => {
-  o = (opts, node, fn) => {
-    let filepath = node.get('filepath'),
-        extname = path.extname(filepath).slice(1),
-        str = node.get('content')
+const adapt = (opts, node, fn) => {
+  let filepath = node.get('filepath'),
+      extname = path.extname(filepath).slice(1),
+      str = node.get('content')
 
-    opts.embedarr.map(embed => {
-      if (~filepath.indexOf(embed.filepath)) {
-        str = embed.content + str
-      }
-    })
-
-    opts.globalarr.map(global => {
-      // module namespace defined on user-given name
-      if (~filepath.indexOf(global.filepath)) {
-        str += '\nif (typeof window === "object") window.:NAME = :UID;'
-          .replace(/:NAME/g, global.name)
-          .replace(/:UID/g, scrounge_uid.sanitised(node.get('uid')))
-      }
-    })
-
-    if (extname && typeof o[extname] === 'function') {
-      o[extname](opts, node, str, fn)
-    } else {
-      fn(null, str)
+  opts.embedarr.map(embed => {
+    if (~filepath.indexOf(embed.filepath)) {
+      str = embed.content + str
     }
+  })
+
+  opts.globalarr.map(global => {
+    // module namespace defined on user-given name
+    if (~filepath.indexOf(global.filepath)) {
+      str += '\nif (typeof window === "object") window.:NAME = :UID;'
+        .replace(/:NAME/g, global.name)
+        .replace(/:UID/g, scrounge_uid.sanitised(node.get('uid')))
+    }
+  })
+
+  if (extname && typeof adapt[extname] === 'function') {
+    adapt[extname](opts, node, str, fn)
+  } else {
+    fn(null, str)
   }
+}
 
-  // found in some sources, such as inferno :( discussion,
-  //
-  //   https://github.com/rollup/rollup/issues/208
-  //
-  o.rmNODE_ENV = (() => {
-    let NODE_ENVre = /process\.env\.NODE_ENV/g,
-        NODE_ENV = `'${process.env.NODE_ENV}'`
+// found in some sources, such as inferno :( discussion,
+//
+//   https://github.com/rollup/rollup/issues/208
+//
+const rmNODE_ENV = (() => {
+  let NODE_ENVre = /process\.env\.NODE_ENV/g,
+      NODE_ENV = `'${process.env.NODE_ENV}'`
 
-    return str => NODE_ENVre.test(str)
-      ? str.replace(NODE_ENVre, NODE_ENV)
-      : str
-  })()
+  return str => NODE_ENVre.test(str)
+    ? str.replace(NODE_ENVre, NODE_ENV)
+    : str
+})()
 
-  o.js = (opts, node, str, fn) => {
-    let filepath = node.get('filepath'),
-        modname = scrounge_uid.sanitised(node.get('uid')),
-        skip = opts.skippatharr.some(path => ~filepath.indexOf(path)),
-        iscjs,
-        isesm
+const js = (opts, node, str, fn) => {
+  let filepath = node.get('filepath'),
+      modname = scrounge_uid.sanitised(node.get('uid')),
+      skip = opts.skippatharr.some(path => ~filepath.indexOf(path)),
+      iscjs,
+      isesm
 
-    if (skip)
-      return fn(null, str)
+  if (skip)
+    return fn(null, str)
 
-    babel.transform(str, {
-      compact : opts.iscompress && !skip,
-      plugins : opts.babelpluginarr || [],
-      sourceMaps : opts.sourcemap,
-      sourceFileName : opts.sourceFileName,
-      presets : [
-        //
-        // rather than try to manage many presets and plugins,
-        // this single preset trys to deliver working output for the
-        // given module definition
-        //
-        // do not convert umd or module-deployed esm
-        //
-        [ babelpresetenv, {
-          modules : (
-            node.get('module') === 'umd' || (
-              node.get('module') === 'esm' && opts.deploytype === 'module'))
-            ? false
-            : 'commonjs'
-        } ]
-      ]
-    }, (err, res) => {
-      if (err) {
-        fn(new Error(`[!!!] parse error ${filepath}`))
-      }
+  babel.transform(str, {
+    compact: opts.iscompress && !skip,
+    plugins: opts.babelpluginarr || [],
+    sourceMaps: opts.sourcemap,
+    sourceFileName: opts.sourceFileName,
+    presets: [
+      //
+      // rather than try to manage many presets and plugins,
+      // this single preset trys to deliver working output for the
+      // given module definition
+      //
+      // do not convert umd or module-deployed esm
+      //
+      [ babelpresetenv, {
+        modules: (
+          node.get('module') === 'umd' || (
+            node.get('module') === 'esm' && opts.deploytype === 'module'))
+          ? false
+          : 'commonjs'
+      } ]
+    ]
+  }, (err, res) => {
+    if (err) {
+      fn(new Error(`[!!!] parse error ${filepath}`))
+    }
 
-      str = res.code
+    str = res.code
 
-      iscjs = moduletype.cjs(str)
-      isesm = moduletype.esm(str)
+    iscjs = moduletype.cjs(str)
+    isesm = moduletype.esm(str)
 
-      if (moduletype.umd(str)) {
-        throw new Error('[!!!] umd format no-longer supported')
-      } else if (iscjs || isesm) {
-        if (iscjs && !isesm)
-          str = umd(modname, str, { commonJS : true })
+    if (moduletype.umd(str)) {
+      throw new Error('[!!!] umd format no-longer supported')
+    } else if (iscjs || isesm) {
+      if (iscjs && !isesm)
+        str = umd(modname, str, { commonJS: true })
 
-        // build import and require replacement mappings
-        let replace = node.get('outarr').reduce((prev, cur) => {
-          let refname = cur.get('refname'),
-              depname = scrounge_uid.sanitised(cur.get('uid'))
+      // build import and require replacement mappings
+      let replace = node.get('outarr').reduce((prev, cur) => {
+        let refname = cur.get('refname'),
+            depname = scrounge_uid.sanitised(cur.get('uid'))
 
-          // alias allows build to map customm paths values
-          // to the require/import value
-          // aliasarr scenario needs tests
-          opts.aliasarr.map(([ matchname, newname ]) => (
-            newname === refname && (
-              prev.require[matchname] = depname,
-              prev.import[matchname] = `/${depname}.js`)))
+        // alias allows build to map customm paths values
+        // to the require/import value
+        // aliasarr scenario needs tests
+        opts.aliasarr.map(([ matchname, newname ]) => (
+          newname === refname && (
+            prev.require[matchname] = depname,
+            prev.import[matchname] = `/${depname}.js`)))
 
-          prev.require[refname] = depname
-          prev.import[refname] = `./${depname}.js`
+        prev.require[refname] = depname
+        prev.import[refname] = `./${depname}.js`
 
-          return prev
-        }, {
-          require : {},
-          import : {}
-        })
+        return prev
+      }, {
+        require : {},
+        import : {}
+      })
 
-        if (iscjs && !/export default/g.test(str))
-          str = replacerequires(str, replace.require)
+      if (iscjs && !/export default/g.test(str))
+        str = replacerequires(str, replace.require)
 
-        if (isesm)
-          str = replaceimports(str, replace.import)
+      if (isesm)
+        str = replaceimports(str, replace.import)
 
-        str = o.rmNODE_ENV(str)
-      }
+      str = rmNODE_ENV(str)
+    }
 
-      fn(null, str, res.map)
-    })
-  }
+    fn(null, str, res.map)
+  })
+}
 
-  // perform o.js on mjs files
-  o.mjs = o.js
+const ts = (opts, node, str, fn) => {
+  let tsconfig = opts.tsconfig || {},
+      jsstr = typescript.transpileModule(str, tsconfig).outputText
 
-  o.ts = (opts, node, str, fn) => {
-    let tsconfig = opts.tsconfig || {},
-        jsstr = typescript.transpileModule(str, tsconfig).outputText
+  js(opts, node, jsstr, fn)
+}
 
-    o.js(opts, node, jsstr, fn)
-  }
+const css = (opts, node, str, fn) => {
+  fn(null, opts.iscompress ?
+    new Cleancss().minify(str).styles : str)
+}
 
-  // perform o.ts on tsx files
-  o.tsx = o.ts
-
-  o.css = (opts, node, str, fn) => {
-    fn(null, opts.iscompress ?
-      new Cleancss().minify(str).styles : str)
-  }
-
-  return o
-})({})
+export default Object.assign(adapt, {
+  rmNODE_ENV,
+  js,
+  mjs: js,
+  ts,
+  tsx: ts,
+  css
+})
